e757763322db12cb04d0510d0bed856bd2201cf0
Nate Grobe <natepgrobe@gmail.com>
2023-03-13 18:23:09 -0400

    made a simple class out of monitor for use in the ui

--- monitor.py
+++ monitor.py
@@ -1,59 +1,51 @@
 import winreg
 
-class WebcamDetect:
+class WebcamRegHandler:
+    # define registry paths
     REG_KEY = winreg.HKEY_CURRENT_USER
-    # WEBCAM_REG_SUBKEY = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\CapabilityAccessManager\\ConsentStore\\webcam\\NonPackaged"
-    WEBCAM_REG_SUBKEY = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\CapabilityAccessManager\\ConsentStore\\webcam"
-    WEBCAM_TIMESTAMP_VALUE_NAME = "LastUsedTimeStop"
+    WEBCAM_REG_LIST = [
+        "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\CapabilityAccessManager\\ConsentStore\\webcam", 
+        "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\CapabilityAccessManager\\ConsentStore\\webcam\\NonPackaged"
+    ]
 
     def __init__(self):
-        self._regKey = winreg.OpenKey(WebcamDetect.REG_KEY, WebcamDetect.WEBCAM_REG_SUBKEY)
+        self._reg_keys = [winreg.OpenKey(WebcamRegHandler.REG_KEY, reg) for reg in WebcamRegHandler.WEBCAM_REG_LIST]
+        self.active_apps = [] # apps with access to webcam
+        self.c_active = [] # apps currently using webcam
+
+    def findActiveApps(self):
+
+        for reg_key, reg_path in zip(self._reg_keys, WebcamRegHandler.WEBCAM_REG_LIST):
+            subkey_cnt, value_cnt, last_modified = winreg.QueryInfoKey(reg_key)
+
+            for idx in range(subkey_cnt):
+                subkey_name = winreg.EnumKey(reg_key, idx)
+                subkey_full_name = f"{reg_path}\\{subkey_name}"
+                subkey = winreg.OpenKey(WebcamRegHandler.REG_KEY, subkey_full_name)
+
+                try:
+                    ts, _ = winreg.QueryValueEx(subkey, "LastUsedTimeStop")
+
+                    if ts == 0:
+                        self.c_active.append(subkey_name)
+                    else:
+                        self.active_apps.append(subkey_name)
+                except:
+                    pass
 
     def getActiveApps(self):
-        activeApps = []
-        currentlyActive = []
+        return self.active_apps, self.c_active
 
-        subkeyCnt, valueCnt, lastModified = winreg.QueryInfoKey(self._regKey)
-        print(subkeyCnt)
-
-        for idx in range(subkeyCnt):
-            subkeyName = winreg.EnumKey(self._regKey, idx)
-            subkeyFullName = f"{WebcamDetect.WEBCAM_REG_SUBKEY}\\{subkeyName}"
-
-            subkey = winreg.OpenKey(WebcamDetect.REG_KEY, subkeyFullName)
-            try:
-                ts, _ = winreg.QueryValueEx(subkey, WebcamDetect.WEBCAM_TIMESTAMP_VALUE_NAME)
-                activeApps.append(subkeyName)
-
-                if ts == 0:
-                    currentlyActive.append(subkeyName)
-            except:
-                pass
-
-                
-        print("Apps with access:")
-        for i in activeApps:
-            print(i)
-
-        print("\nCurrently active apps:")
-        for i in currentlyActive:
-            print(i)
-
-
-        #     name = subkeyName.replace("#", "/")
-
-        #     print(name, ts)
-
-            # if ts == 0:
-            #     print(subkeyName)
-
-
-
-
-def main():
-    wd = WebcamDetect()
-    wd.getActiveApps()
-
-
+# sample of how to use the class
 if __name__ == '__main__':
-    main()
+    reg_handler = WebcamRegHandler()
+    reg_handler.findActiveApps()
+    active, c_active = reg_handler.getActiveApps()
+
+    print("Active Apps:")
+    for i in active:
+        print(i)
+
+    print("\nCurrently Active App:")
+    for i in c_active:
+        print(i)
