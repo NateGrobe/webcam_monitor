1495d90dce79db4df963916bf4a16060a9d5841f
Konen <colekahkonen@gmail.com>
2023-03-14 19:50:34 -0400

    rewritten in Custom TKinter

--- tk.py
+++ tk.py
@@ -4,213 +4,92 @@ import time
 from datetime import datetime
 import chime
 from utils import write_to_log
-
-import tkinter
 import customtkinter
 
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
-# class App:
-#     def __init__(self, window):
-#         self.window = window
-#         self.window.title("Webcam Monitor")
-
-#         # create Active Apps label and listbox
-#         self.active_label = Label(window, text="Active Apps")
-#         self.active_label.grid(row=0, column=0, padx=10, pady=10, sticky="w")
-
-#         self.active_list = Listbox(window, width=50)
-#         self.active_list.grid(row=1, column=0, padx=10, pady=10)
-
-#         # create Currently Active App label and listbox
-#         self.c_active_label = Label(window, text="Currently Active App")
-#         self.c_active_label.grid(row=0, column=1, padx=10, pady=10, sticky="w")
-
-#         self.c_active_list = Listbox(window, width=50)
-#         self.c_active_list.grid(row=1, column=1, padx=10, pady=10)
-
-#         # create Start and Stop buttons
-#         self.startbtn = Button(window, text='Start', height=2, width=10, command=self.start_monitor)
-#         self.startbtn.grid(row=2, column=0, padx=10, pady=10)
-
-#         self.stopbtn = Button(window, text='Stop', height=2, width=10, command=self.stop_monitor)
-#         self.stopbtn.grid(row=2, column=1, padx=10, pady=10)
-
-#         self.active_tracker = {}
-
-#         self.window.mainloop()
-
-#     def start_monitor(self):
-#         self.start = True
-#         self.monitor()
-
-#     def stop_monitor(self):
-#         self.start = False
-
-#     def monitor(self):
-#         if self.start:
-#             reg_handler = WebcamRegHandler()
-#             active, c_active = reg_handler.getActiveApps()
-
-#             self.active_list.delete(0, END)
-#             self.c_active_list.delete(0, END)
-
-#             for p in active:
-#                 if p in self.active_tracker.keys():
-#                     time_active = time.time() - self.active_tracker[p]
-#                     log_str = f"{p} stopped after {time_active:.2f}s\n" 
-#                     write_to_log(log_str)
-#                     self.active_tracker.pop(p)
-#                 self.active_list.insert(END, p)
-
-#             for p in c_active:
-#                 if p not in self.active_tracker.keys():
-#                     log_str = f"{p} started at {datetime.now()}\n"
-#                     write_to_log(log_str)
-#                     self.active_tracker[p] = time.time()
-#                     chime.success()
-#                 self.c_active_list.insert(END, p)
-            
-#             self.window.after(1000, self.monitor)
-
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
+        self.start = False
+
         # configure window
         self.title("Webcam Monitor")
-        self.geometry(f"{1100}x{580}")
+        self.geometry(f"{1100}x{725}")
 
         # configure grid layout (4x4)
         self.grid_columnconfigure(1, weight=1)
         self.grid_columnconfigure((2, 3), weight=0)
         self.grid_rowconfigure((0, 1, 2), weight=1)
 
-        # create sidebar frame with widgets
+        # create sidebar frame with widgets  OPTIONS COL
         self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
         self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
         self.sidebar_frame.grid_rowconfigure(4, weight=1)
         self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="Options", font=customtkinter.CTkFont(size=20, weight="bold"))
         self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
-        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, text="Check stats", command=self.sidebar_button_event)
-        self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
-        # self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event)
-        # self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
-        # self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event)
-        # self.sidebar_button_3.grid(row=3, column=0, padx=20, pady=10)
+       
+        # START BUTTON
+        self.startbtn = customtkinter.CTkButton(self.sidebar_frame, text="Start Monitor", command=self.handle_start_stop)
+        self.startbtn.grid(row=1, column=0, padx=20, pady=10)
+
+        # Logging
+        self.logging_cb = customtkinter.CTkCheckBox(self.sidebar_frame)
+        self.logging_cb.grid(row=1, column=0, pady=(100, 0), padx=20, sticky="n")
+       
+        # LIGHT AND DARK MODE ETC
         self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
         self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
         self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Light", "Dark", "System"],
                                                                        command=self.change_appearance_mode_event)
         self.appearance_mode_optionemenu.grid(row=6, column=0, padx=20, pady=(10, 10))
-        self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="UI Scaling:", anchor="w")
-        self.scaling_label.grid(row=7, column=0, padx=20, pady=(10, 0))
-        self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"],
-                                                               command=self.change_scaling_event)
-        self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
 
-        # create main entry and button
-        self.entry = customtkinter.CTkEntry(self, placeholder_text="CTkEntry")
-        self.entry.grid(row=3, column=1, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
 
-        self.main_button_1 = customtkinter.CTkButton(master=self, fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"))
-        self.main_button_1.grid(row=3, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
+# Widgets
+        # Apps with access
+        self.active_list_frame = customtkinter.CTkFrame(self, corner_radius=10, width=500, height=250)
+        self.active_list_frame.grid(row=0, column=1, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
-        # create textbox
-        # self.textbox = customtkinter.CTkTextbox(self, width=250)
-        # self.textbox.grid(row=0, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
+        self.active_list_label = customtkinter.CTkLabel(self.active_list_frame, text="Apps with access", font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.active_list_label.grid(row=0, column=1, columnspan=1, padx=10, pady=10, sticky="")
+       
+        self.active_list = customtkinter.CTkTextbox(self.active_list_frame, width=400, height=400, cursor=None, yscrollcommand=True, corner_radius=10)
+        self.active_list.grid(row=2, column=1, padx=(10, 10), pady=(10, 10), sticky="nsew")
+        
 
-        # create tabview
-        self.tabview = customtkinter.CTkTabview(self, width=250)
-        self.tabview.grid(row=0, column=2, padx=(20, 0), pady=(20, 0), sticky="nsew")
-        self.tabview.add("CTkTabview")
-        self.tabview.add("Tab 2")
-        self.tabview.add("Tab 3")
-        self.tabview.tab("CTkTabview").grid_columnconfigure(0, weight=1)  # configure grid of individual tabs
-        self.tabview.tab("Tab 2").grid_columnconfigure(0, weight=1)
+        # Output log
+        self.log_frame = customtkinter.CTkFrame(self, corner_radius=10, width=250, height=250)
+        self.log_frame.grid(row=0, column=2, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
-        self.optionmenu_1 = customtkinter.CTkOptionMenu(self.tabview.tab("CTkTabview"), dynamic_resizing=False,
-                                                        values=["Value 1", "Value 2", "Value Long Long Long"])
-        self.optionmenu_1.grid(row=0, column=0, padx=20, pady=(20, 10))
-        self.combobox_1 = customtkinter.CTkComboBox(self.tabview.tab("CTkTabview"),
-                                                    values=["Value 1", "Value 2", "Value Long....."])
-        self.combobox_1.grid(row=1, column=0, padx=20, pady=(10, 10))
-        self.string_input_button = customtkinter.CTkButton(self.tabview.tab("CTkTabview"), text="Open CTkInputDialog",
-                                                           command=self.open_input_dialog_event)
-        self.string_input_button.grid(row=2, column=0, padx=20, pady=(10, 10))
-        self.label_tab_2 = customtkinter.CTkLabel(self.tabview.tab("Tab 2"), text="CTkLabel on Tab 2")
-        self.label_tab_2.grid(row=0, column=0, padx=20, pady=20)
+        self.log_label = customtkinter.CTkLabel(self.log_frame, text="Output Log", font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.log_label.grid(row=0, column=1, columnspan=1, padx=10, pady=10, sticky="")
+       
+        self.log_list = customtkinter.CTkTextbox(self.log_frame, width=400, height=400, cursor=None, yscrollcommand=True, corner_radius=10)
+        self.log_list.grid(row=2, column=1, padx=(10, 10), pady=(10, 10), sticky="nsew")
 
-        # create radiobutton frame
-        # self.radiobutton_frame = customtkinter.CTkFrame(self)
-        # self.radiobutton_frame.grid(row=0, column=3, padx=(20, 20), pady=(20, 0), sticky="nsew")
-        # self.radio_var = tkinter.IntVar(value=0)
-        # self.label_radio_group = customtkinter.CTkLabel(master=self.radiobutton_frame, text="CTkRadioButton Group:")
-        # self.label_radio_group.grid(row=0, column=2, columnspan=1, padx=10, pady=10, sticky="")
-        # self.radio_button_1 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var, value=0)
-        # self.radio_button_1.grid(row=1, column=2, pady=10, padx=20, sticky="n")
-        # self.radio_button_2 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var, value=1)
-        # self.radio_button_2.grid(row=2, column=2, pady=10, padx=20, sticky="n")
-        # self.radio_button_3 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var, value=2)
-        # self.radio_button_3.grid(row=3, column=2, pady=10, padx=20, sticky="n")
 
-        # create slider and progressbar frame
-        # self.slider_progressbar_frame = customtkinter.CTkFrame(self, fg_color="transparent")
-        # self.slider_progressbar_frame.grid(row=1, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
-        # self.slider_progressbar_frame.grid_columnconfigure(0, weight=1)
-        # self.slider_progressbar_frame.grid_rowconfigure(4, weight=1)
-        # self.seg_button_1 = customtkinter.CTkSegmentedButton(self.slider_progressbar_frame)
-        # self.seg_button_1.grid(row=0, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
-        # self.progressbar_1 = customtkinter.CTkProgressBar(self.slider_progressbar_frame)
-        # self.progressbar_1.grid(row=1, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
-        # self.progressbar_2 = customtkinter.CTkProgressBar(self.slider_progressbar_frame)
-        # self.progressbar_2.grid(row=2, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
-        # self.slider_1 = customtkinter.CTkSlider(self.slider_progressbar_frame, from_=0, to=1, number_of_steps=4)
-        # self.slider_1.grid(row=3, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
-        # self.slider_2 = customtkinter.CTkSlider(self.slider_progressbar_frame, orientation="vertical")
-        # self.slider_2.grid(row=0, column=1, rowspan=5, padx=(10, 10), pady=(10, 10), sticky="ns")
-        # self.progressbar_3 = customtkinter.CTkProgressBar(self.slider_progressbar_frame, orientation="vertical")
-        # self.progressbar_3.grid(row=0, column=2, rowspan=5, padx=(10, 20), pady=(10, 10), sticky="ns")
+        # Currently Active
+        self.current_list_frame = customtkinter.CTkFrame(self, corner_radius=10, width=500, height=100)
+        self.current_list_frame.grid(row=1, column=1, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
-        # create scrollable frame
-        self.scrollable_frame = customtkinter.CTkScrollableFrame(self, label_text="Apps With Access")
-        self.scrollable_frame.grid(row=1, column=2, padx=(20, 0), pady=(20, 0), sticky="nsew")
-        self.scrollable_frame.grid_columnconfigure(0, weight=1)
-        self.scrollable_frame_switches = []
-        for i in range(100):
-            switch = customtkinter.CTkSwitch(master=self.scrollable_frame, text=f"CTkSwitch {i}")
-            switch.grid(row=i, column=0, padx=10, pady=(0, 20))
-            self.scrollable_frame_switches.append(switch)
+        self.current_list_label = customtkinter.CTkLabel(self.current_list_frame, text="Currently Active", font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.current_list_label.grid(row=0, column=1, columnspan=1, padx=10, pady=10, sticky="")
 
-        # create checkbox and switch frame
-        self.checkbox_slider_frame = customtkinter.CTkFrame(self)
-        self.checkbox_slider_frame.grid(row=1, column=3, padx=(20, 20), pady=(20, 0), sticky="nsew")
-        self.checkbox_1 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
-        self.checkbox_1.grid(row=1, column=0, pady=(20, 0), padx=20, sticky="n")
-        self.checkbox_2 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
-        self.checkbox_2.grid(row=2, column=0, pady=(20, 0), padx=20, sticky="n")
-        self.checkbox_3 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
-        self.checkbox_3.grid(row=3, column=0, pady=20, padx=20, sticky="n")
+        self.current_list = customtkinter.CTkTextbox(self.current_list_frame, width=400, height=100, cursor=None, yscrollcommand=True, corner_radius=10)
+        self.current_list.grid(row=1, column=1, padx=(10, 10), pady=(10, 10), sticky="nsew")
+
 
         # set default values
-        # self.sidebar_button_3.configure(state="disabled", text="Disabled CTkButton")
-        self.checkbox_3.configure(state="disabled")
-        self.checkbox_1.select()
-        self.scrollable_frame_switches[0].select()
-        self.scrollable_frame_switches[4].select()
-        # self.radio_button_3.configure(state="disabled")
         self.appearance_mode_optionemenu.set("Dark")
-        self.scaling_optionemenu.set("100%")
-        self.optionmenu_1.set("CTkOptionmenu")
-        self.combobox_1.set("CTkComboBox")
-        # self.slider_1.configure(command=self.progressbar_2.set)
-        # self.slider_2.configure(command=self.progressbar_3.set)
-        # self.progressbar_1.configure(mode="indeterminnate")
-        # self.progressbar_1.start()
-        # self.textbox.insert("0.0", "CTkTextbox\n\n" + "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.\n\n" * 20)
-        # self.seg_button_1.configure(values=["CTkSegmentedButton", "Value 2", "Value 3"])
-        # self.seg_button_1.set("Value 2")
+        self.logging_cb_state = True
+        self.logging_cb.toggle()
+        self.active_tracker = {}
+        
+        self.mainloop()
+
+        
 
     def open_input_dialog_event(self):
         dialog = customtkinter.CTkInputDialog(text="Type in a number:", title="CTkInputDialog")
@@ -219,12 +98,70 @@ class App(customtkinter.CTk):
     def change_appearance_mode_event(self, new_appearance_mode: str):
         customtkinter.set_appearance_mode(new_appearance_mode)
 
-    def change_scaling_event(self, new_scaling: str):
-        new_scaling_float = int(new_scaling.replace("%", "")) / 100
-        customtkinter.set_widget_scaling(new_scaling_float)
+
 
     def sidebar_button_event(self):
         print("sidebar_button click")
+
+    def handle_start_stop(self):
+
+        if not self.start:
+            self.start = True
+            self.startbtn.configure(text="Stop")
+            self.start_monitor()
+        else:
+            self.start = True
+            self.startbtn.configure(text="Start")
+            self.stop_monitor()
+
+    def start_monitor(self):
+        self.start = True
+        self.monitor()
+
+    def stop_monitor(self):
+        self.start = False
+
+
+    def toggle_logging(self):
+        self.logging_cb_state = not self.logging_cb_state
+        
+    def monitor(self):
+        if self.start:
+            reg_handler = WebcamRegHandler()
+            active, c_active = reg_handler.getActiveApps()
+            self.active_list.delete('1.0',END)
+            self.current_list.delete('1.0',END)
+            print("\n\nActive Apps:")
+            for p in active:
+                if p in self.active_tracker.keys():
+                    time_active = time.time() - self.active_tracker[p]
+                    log_str = f"{p} stopped after {time_active:.2f}s\n" 
+                    write_to_log(log_str)
+                    if self.logging_cb_state:
+                        self.log_list.insert(END, f"\n{log_str}")
+                    self.active_tracker.pop(p)
+                    
+                print(p)
+                self.active_list.insert(END,f"\n{p}")
+                self.active_list.see(END)
+                
+
+            print("\nCurrently Active App:")
+            for p in c_active:
+                if p not in self.active_tracker.keys():
+                    log_str = f"{p} started at {datetime.now()}\n"
+                    write_to_log(log_str)
+                    if self.logging_cb_state:
+                        self.log_list.insert(END, f"\n{log_str}")
+                    self.active_tracker[p] = time.time()
+                    chime.success()
+                    
+                print(p)
+                self.current_list.insert(END, f"\n{c_active}")
+                self.current_list.see(END)  
+                
+            
+            self.after(1000, self.monitor)
 
 
 if __name__ == '__main__':
