fb4d62a5732f532d54d276efbca67aa96974e9aa
Krystian Rusin <36743674+KrystianRusin@users.noreply.github.com>
2023-03-14 16:28:34 -0400

    Added lists for active and inactive apps to tkinter gui

--- tk.py
+++ tk.py
@@ -10,12 +10,26 @@ class App:
         self.window = window
         self.window.title("Webcam Monitor")
 
-        self.start = False
+        # create Active Apps label and listbox
+        self.active_label = Label(window, text="Active Apps")
+        self.active_label.grid(row=0, column=0, padx=10, pady=10, sticky="w")
+
+        self.active_list = Listbox(window, width=50)
+        self.active_list.grid(row=1, column=0, padx=10, pady=10)
+
+        # create Currently Active App label and listbox
+        self.c_active_label = Label(window, text="Currently Active App")
+        self.c_active_label.grid(row=0, column=1, padx=10, pady=10, sticky="w")
+
+        self.c_active_list = Listbox(window, width=50)
+        self.c_active_list.grid(row=1, column=1, padx=10, pady=10)
+
+        # create Start and Stop buttons
         self.startbtn = Button(window, text='Start', height=2, width=10, command=self.start_monitor)
-        self.startbtn.pack()
+        self.startbtn.grid(row=2, column=0, padx=10, pady=10)
 
         self.stopbtn = Button(window, text='Stop', height=2, width=10, command=self.stop_monitor)
-        self.stopbtn.pack()
+        self.stopbtn.grid(row=2, column=1, padx=10, pady=10)
 
         self.active_tracker = {}
 
@@ -33,23 +47,24 @@ class App:
             reg_handler = WebcamRegHandler()
             active, c_active = reg_handler.getActiveApps()
 
-            print("\n\nActive Apps:")
+            self.active_list.delete(0, END)
+            self.c_active_list.delete(0, END)
+
             for p in active:
                 if p in self.active_tracker.keys():
                     time_active = time.time() - self.active_tracker[p]
                     log_str = f"{p} stopped after {time_active:.2f}s\n" 
                     write_to_log(log_str)
                     self.active_tracker.pop(p)
-                print(p)
+                self.active_list.insert(END, p)
 
-            print("\nCurrently Active App:")
             for p in c_active:
                 if p not in self.active_tracker.keys():
                     log_str = f"{p} started at {datetime.now()}\n"
                     write_to_log(log_str)
                     self.active_tracker[p] = time.time()
                     chime.success()
-                print(p)
+                self.c_active_list.insert(END, p)
             
             self.window.after(1000, self.monitor)
 
